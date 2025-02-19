6ba25047a7494eb3037b1e03e8b46fb3211c549a
fortidemoscloud <fortidemoscloud@proton.me>
2025-02-19 19:16:46 +0100

    first commit

---
+++ basic_fgt-ha-xlb-app-sec/main.tf
@@ -0,0 +1,112 @@
+
+variable "custom_vars" {
+  description = "Custom variables"
+  type = object({
+    prefix         = optional(string, "fgt-ha-xlb")
+    region         = optional(string, "spaincentral")
+    fgt_version    = optional(string, "7.4.6")
+    license_type   = optional(string, "payg")
+    fgt_size       = optional(string, "Standard_F4s")
+    fgt_vnet_cidr  = optional(string, "172.10.0.0/23")
+    admin_username = optional(string, "azureadmin")
+    k8s_size       = optional(string, "Standard_B2ls_v2")
+    k8s_version    = optional(string, "1.31")
+    tags           = optional(map(string), { "Deploy" = "CloudLab AWS", "Project" = "CloudLab" })
+  })
+  default = {}
+}
+
+# ----------------------------------------------------------------------------------------
+# Module to deploy de FortiGate cluster items
+# - FortiGate VNET, Subnet, routes, NSG ...
+# - FortiGate HA pair VM
+# ----------------------------------------------------------------------------------------
+module "fgt-ha-xlb" {
+  source  = "jmvigueras/ftnt-azure-modules/azure//examples/basic_fgt-ha-xlb"
+  version = "0.0.7"
+
+  prefix   = var.custom_vars["prefix"]
+  location = var.custom_vars["region"]
+
+  admin_username = var.custom_vars["admin_username"]
+
+  license_type = var.custom_vars["license_type"]
+  fgt_size     = var.custom_vars["fgt_size"]
+  fgt_version  = var.custom_vars["fgt_version"]
+
+  fgt_vnet_cidr = var.custom_vars["fgt_vnet_cidr"]
+
+  tags = var.custom_vars["tags"]
+}
+
+# ----------------------------------------------------------------------------------------
+# K8s Server
+# ----------------------------------------------------------------------------------------
+module "k8s" {
+  source  = "jmvigueras/ftnt-azure-modules/azure//modules/vm"
+  version = "0.0.7"
+
+  prefix   = var.custom_vars["prefix"]
+  location = var.custom_vars["region"]
+
+  resource_group_name = module.fgt-ha-xlb.resource_group_name
+
+  admin_username = var.custom_vars["admin_username"]
+  rsa-public-key = module.fgt-ha-xlb.public_key_openssh
+  user_data      = local.k8s_user_data
+
+  vm_size = var.custom_vars["k8s_size"]
+
+  subnet_id   = module.fgt-ha-xlb.subnet_ids["bastion"]
+  subnet_cidr = module.fgt-ha-xlb.subnet_cidrs["bastion"]
+
+  tags = var.custom_vars["tags"]
+}
+
+locals {
+  # K8S configuration and APP deployment
+  k8s_deployment = templatefile("./templates/k8s-dvwa-swagger.yaml", {
+    dvwa_nodeport    = "31000"
+    swagger_nodeport = "31001"
+    swagger_host     = module.fgt-ha-xlb.fgt_ips["fgt1"]["public"]
+    swagger_url      = "http://${module.fgt-ha-xlb.fgt_ips["fgt1"]["public"]}:31001"
+    }
+  )
+  k8s_user_data = templatefile("./templates/k8s.sh", {
+    k8s_version    = var.custom_vars["k8s_version"]
+    linux_user     = var.custom_vars["admin_username"]
+    k8s_deployment = local.k8s_deployment
+    }
+  )
+}
+
+# ----------------------------------------------------------------------------------------
+# Outputs
+# ----------------------------------------------------------------------------------------
+output "fgt" {
+  value = module.fgt-ha-xlb.fgt
+}
+
+output "k8s" {
+  value = module.k8s.vm
+}
+
+# ----------------------------------------------------------------------------------------
+# Provider
+# ----------------------------------------------------------------------------------------
+# Define variable subscription_id to use terraform.tfvars
+variable "subscription_id" {}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = var.subscription_id
+}
+
+# Prevent Terraform warning for backend config
+terraform {
+  backend "s3" {}
+}
