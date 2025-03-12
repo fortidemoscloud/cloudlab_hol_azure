2d54ad848f2ea96bb4d5a46fefd99d9a3d74ec4a
fortidemoscloud <fortidemoscloud@proton.me>
2025-03-12 18:36:23 +0100

    modify code to adapt variables for autodeploy

--- basic_fgt-ha-xlb-app-sec/main.tf
+++ basic_fgt-ha-xlb-app-sec/main.tf
@@ -102,7 +102,11 @@ output "k8s" {
 # Provider
 # ----------------------------------------------------------------------------------------
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 # Prevent Terraform warning for backend config
