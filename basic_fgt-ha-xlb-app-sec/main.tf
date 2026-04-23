4f7483ac86110f5e59ee8809f8ffaf221aa13cb1
fortidemoscloud <fortidemoscloud@proton.me>
2026-04-23 23:30:36 +0200

    fix: subscription var not necessary in provider

--- basic_fgt-ha-xlb-app-sec/main.tf
+++ basic_fgt-ha-xlb-app-sec/main.tf
@@ -101,15 +101,12 @@ output "k8s" {
 # ----------------------------------------------------------------------------------------
 # Provider
 # ----------------------------------------------------------------------------------------
-variable "subscription_id" {}
-
 provider "azurerm" {
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = var.subscription_id
 }
 
 # Prevent Terraform warning for backend config
